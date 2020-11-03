d861643f28ffc50c91e2477869a3c456b98abdd6
Victor Cordeiro <victor.hugo@systemhope.com.br>
2020-11-03 19:49:05 -0300

    Alterações no splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StatusBar } from 'react-native'
 import 'react-native-gesture-handler'
+import SplashScreen from 'react-native-splash-screen'
 import Routes from './routes'
 
 
 const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
   return (
     <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle="light-content" backgroundColor="#f11f22" />
       <Routes />
     </NavigationContainer>
   )
--- src/pages/DashboardPage/styles.ts
+++ src/pages/DashboardPage/styles.ts
@@ -1,38 +1,38 @@
-import styled from 'styled-components/native';
+import styled from 'styled-components/native'
 
 export const ContainerMarvel = styled.View`
   flex: 1;
-  background-color:#fe4646;
+  background-color: #f11f22;
   align-items: center;
   justify-content: center;
   text-align: center;
 `
 
 export const MarvelText = styled.Text`
-color: #fff;
-font-weight: bold;
-justify-content: center;
-font-size: 58px;
+  color: #fff;
+  font-weight: bold;
+  justify-content: center;
+  font-size: 58px;
 `
 export const MarvelComics = styled.Text`
-color: #fff;
-justify-content: center;
-font-size: 32px;
+  color: #fff;
+  justify-content: center;
+  font-size: 32px;
 `
 
 export const RecipeMarvel = styled.View`
-padding: 0 30px;
-width: 100%;
+  padding: 0 30px;
+  width: 100%;
 `
 
 export const ButtonMarvel = styled.TouchableOpacity`
-height: 50px;
-width: 100%;
-margin-top: 50px;
-align-items: center;
-justify-content: center;
+  height: 50px;
+  width: 100%;
+  margin-top: 50px;
+  align-items: center;
+  justify-content: center;
 `
 export const TextMarvel = styled.Text`
-color: #fff;
-font-size: 16px;
+  color: #fff;
+  font-size: 16px;
 `
